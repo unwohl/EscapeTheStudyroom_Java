0848018cc6a2f4aae432ccac2d5ee04b27fcd046
Altanoras <jokaphi@gmx.de>
2019-03-19 16:05:53 +0100

    Yeet

--- Project/EscapeTheStudyroom/src/Game.java
+++ Project/EscapeTheStudyroom/src/Game.java
@@ -1,2 +1,8 @@
+import Items.*;
+
 public class Game {
+    public static void main(String[] args){
+        Thing shelf = new Thing();
+        UseableThing drawer = new UseableThing();
+    }
 }
--- Project/EscapeTheStudyroom/src/Items/Thing.java
+++ Project/EscapeTheStudyroom/src/Items/Thing.java
@@ -1,4 +1,27 @@
 package Items;
 
 public class Thing {
+    public static int ThingCount = 0;
+
+    String _description = "No description available...";
+    int _ID = 0;
+
+    public Thing(String arg1){ //arg1 = description
+        _description = arg1;
+        ThingCount++;
+        _ID = ThingCount;
+    }
+
+    public Thing(){
+        ThingCount++;
+        _ID = ThingCount;
+    }
+
+    public String examine(){
+        return _description;
+    }
+
+    public int getID(){
+        return _ID;
+    }
 }
