5df7f7b52f03da67d03b04379a0a0379e3159dc2
ExceIsior <48092243+ExceIsior@users.noreply.github.com>
2019-03-23 14:56:15 +0100

    lo

--- Project/EscapeTheStudyroom/src/Game/StudyRoom.java
+++ Project/EscapeTheStudyroom/src/Game/StudyRoom.java
@@ -8,4 +8,9 @@ public class StudyRoom extends Room{
     String _description = "Your study. A desk lamp dimly illuminates the following Items:";
     int room_Id = 0;
 
+    public StudyRoom()
+    {
+        Room
+    }
+
 }
