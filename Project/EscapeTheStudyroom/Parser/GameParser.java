e4c7232c9b291c8a3941d5bfc22e7373adec2f4f
Dawinartor <david.melamed@web.de>
2019-04-05 20:55:10 +0200

    Achtung ich pushe
    Yied

---
+++ Project/EscapeTheStudyroom/Parser/GameParser.java
@@ -0,0 +1,23 @@
+package Parser;
+
+import java.io.*;
+
+public class GameParser {
+    
+	String [] zerlegteEingabe;
+	BufferedReader eingabe = new BufferedReader(new InputStreamReader(System.in));
+
+    public GameParser () throws IOException {
+    	
+        String a = eingabe.readLine();
+        
+    }
+
+    public void verarbeiteEingabe () //Switch Bedingung
+    {
+       
+    }
+}
+
+
+
