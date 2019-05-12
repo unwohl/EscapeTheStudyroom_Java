6d660c0639407ec8b4b7d4901b3450c637db47e1
Altanoras <jokaphi@gmx.de>
2019-05-12 13:32:02 +0200

    Added more documentation

--- Project/EscapeTheStudyroom/src/Items/Inventory.java
+++ Project/EscapeTheStudyroom/src/Items/Inventory.java
@@ -18,6 +18,10 @@ public class Inventory {
         inventory.add(thing);
     }
 
+    /**
+     * Removes an Item with the ID from the Inventory of the Player.
+     * @param ID the ID of the Object you want to remove from the Inventory.
+     */
     public void removeFromInventorybyID(int ID){
         for (Thing thing : inventory) {
             if(thing._ID == ID){
@@ -26,6 +30,11 @@ public class Inventory {
         }
     }
 
+    /**
+     * Checks if an Item with your ID is in the Inventory of the Player.
+     * @param ID The ID of the Item you want to verify.
+     * @return Returns TRUE if the item is in the Inventory of the Player.
+     */
     public boolean itemInInventory(int ID){
         for (Thing thing : inventory) {
             if(thing._ID == ID){
@@ -35,6 +44,11 @@ public class Inventory {
         return false;
     }
 
+    /**
+     * Checks if a THING is in the Inventory of the Player.
+     * @param thing the THING you want to verify.
+     * @return returns TRUE if the THING is in the Inventory of the Player.
+     */
     public boolean itemInInventory(Thing thing){
         if(inventory.contains(thing)){
             return true;
