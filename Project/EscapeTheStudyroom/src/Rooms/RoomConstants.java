492b03d82858403457297ee8cf59d7acabe33acd
Altanoras <jokaphi@gmx.de>
2019-05-18 15:10:17 +0200

    added \n's

--- Project/EscapeTheStudyroom/src/Rooms/RoomConstants.java
+++ Project/EscapeTheStudyroom/src/Rooms/RoomConstants.java
@@ -10,7 +10,8 @@ public interface RoomConstants {
 
     public static final int BEDROOM_ID = 0;
     public static final String BEDROOM_NAME = "bedroom";
-    public static final String BEDROOM_DESCRIPTION = "You enter the bedroom and see the bed in which you woke up, aswell as your nightstand, your cupboard, a dildo and your favorite rag";
+    public static final String BEDROOM_DESCRIPTION = "You enter the bedroom and see the bed in which you woke up, aswell as your nightstand," +
+            "\n your cupboard, a dildo and your favorite rag";
     public static final boolean BEDROOM_UNLOCKED_ = true;
     public static final List<Thing> BEDROOM_ITEMS_ = new ArrayList<Thing>();
     public static final String[] BEDROOM_ADJACENTS = {"livingroom"};
@@ -26,14 +27,16 @@ public interface RoomConstants {
 
     public static final int BATHROOM_ROOM_ID = 2;
     public static final String BATHROOM_NAME = "bathroom";
-    public static final String BATHROOM_DESCRIPTION = "You enter the bathroom and see your shower that seems to have been used recently, next to the toilet is the bin and your sink also seems to be in its usual place.";
+    public static final String BATHROOM_DESCRIPTION = "You enter the bathroom and see your shower that seems to have been used recently, " +
+            "\nnext to the toilet is the bin and your sink also seems to be in its usual place.";
     public static final boolean BATHROOM_UNLOCKED_ = false;
     public static final List<Thing> BATHROOM_ITEMS_ = new ArrayList<Thing>();
     public static final String[] BATHROOM_ADJACENTS = {"livingroom"};
 
     public static final int TERRACE_ID = 3;
     public static final String TERRACE_NAME = "terrace";
-    public static final String TERRACE_DESCRIPTION = "You step out onto the terrace and INSTANTLY notice a big puddle of blood under your chair thats flowing towards your parsley plant and dripping off under the rail.";
+    public static final String TERRACE_DESCRIPTION = "You step out onto the terrace and INSTANTLY notice a big puddle of blood under your " +
+            "\nchair thats flowing towards your parsley plant and dripping off under the rail.";
     public static final boolean TERRACE_UNLOCKED_ = false;
     public static final List<Thing> TERRACE_ITEMS_ = new ArrayList<Thing>();
     public static final String[] TERRACE_ADJACENTS = {"livingroom"};
